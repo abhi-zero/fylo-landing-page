75164f579d7791a8d81b1e125e8b3a263b8850bc
abhi-zero <abhi.0zero@gmail.com>
2025-11-06 15:51:45 +0530

    feat: implement GSAP animation for mobile navigation toggle

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { gsap } from "gsap";
+const menuButton = document.querySelector('.menu-toggle');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const navToggleTl = gsap.timeline();
 
-setupCounter(document.querySelector('#counter'))
+navToggleTl.from('.mobile-nav',{
+    height: "0",
+    opacity: 0,
+    ease: "power3.inOut"
+})
+
+navToggleTl.pause()
+menuButton.addEventListener('click', ()=> {
+    if(menuButton.classList.contains("open")){
+        navToggleTl.reverse()
+        menuButton.classList.remove('open')
+    }else{
+        
+        navToggleTl.play()
+        menuButton.classList.add('open')
+    }
+})
