c8593c5d5f024d83b4437f3f85050f74b8c8fa6f
abhi-zero <abhi.0zero@gmail.com>
2025-11-06 22:38:00 +0530

    feat: update GSAP animations in anime.js for improved visual effects

--- src/anime.js
+++ src/anime.js
@@ -3,19 +3,44 @@ import { gsap } from "gsap";
 const navIntroTl = gsap.timeline({
   defaults: {
     ease: "power1.inOut",
-    duration: 1,
-    opacity: 0,
+   
   },
 });
 
 navIntroTl
   .from(".logo", {
     y: -50,
+    duration: 1,
+    opacity: 0,
   })
   .from(".nav-li", {
     yPercent: -100,
     stagger: 0.5,
+     duration: 1,
+    opacity: 0,
   },"start")
   .from(".menu-toggle", {
     yPercent: -100,
-  },"start");
+     duration: 1,
+    opacity: 0,
+  },"start")
+  .from('.hero-img',{
+    y:60,
+    opacity:0,
+    scale:.85,
+    duration: 1,
+  },"-=.2")
+  .from(".hero-heading",{
+    y:40,
+    opacity:0,
+ duration: 1,
+  },"-=.2")
+  .from(".hero-subtext",{
+    y:30,
+    opacity:0
+  },"-=.2")
+  .fromTo(".cta",
+  { y: 30, opacity: 0 },
+  { y: 0, opacity: 1, duration: .5 },
+  "-=.2"
+);
