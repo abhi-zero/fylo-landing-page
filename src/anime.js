d92e3a1a366e161fff246979357233cfacdbb588
abhi-zero <abhi.0zero@gmail.com>
2025-11-08 16:57:32 +0530

    refactor: clean up animation code for improved readability and consistency

--- src/anime.js
+++ src/anime.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 const navIntroTl = gsap.timeline({
   defaults: {
     ease: "power1.inOut",
-   
+
   },
 });
 
@@ -19,38 +19,38 @@ navIntroTl
   .from(".nav-li", {
     yPercent: -100,
     stagger: 0.5,
-     duration: 1,
+    duration: 1,
     opacity: 0,
-  },"start")
+  }, "start")
   .from(".menu-toggle", {
     yPercent: -100,
-     duration: 1,
-    opacity: 0,
-  },"start")
-  .from('.hero-img',{
-    y:60,
-    opacity:0,
-    scale:.85,
     duration: 1,
-  },"-=.2")
-  .from(".hero-heading",{
-    y:40,
-    opacity:0,
- duration: 1,
-  },"-=.2")
-  .from(".hero-subtext",{
-    y:30,
-    opacity:0
-  },"-=.2")
+    opacity: 0,
+  }, "start")
+  .from('.hero-img', {
+    y: 60,
+    opacity: 0,
+    scale: .85,
+    duration: 1,
+  }, "-=.2")
+  .from(".hero-heading", {
+    y: 40,
+    opacity: 0,
+    duration: 1,
+  }, "-=.2")
+  .from(".hero-subtext", {
+    y: 30,
+    opacity: 0
+  }, "-=.2")
   .fromTo(".cta",
-  { y: 60, opacity: 0 },
-  { y: 0, opacity: 1, duration: 1 }
-);
+    { y: 60, opacity: 0 },
+    { y: 0, opacity: 1, duration: 1 }
+  );
 
 
 
 gsap.from(".feature", {
-  y:50,
+  y: 50,
   opacity: 0,
   stagger: .3,
   scrollTrigger: {
@@ -73,27 +73,27 @@ const productivityTL = gsap.timeline({
   }
 })
 
-productivityTL.from('.productive-img',{
+productivityTL.from('.productive-img', {
   x: -60,
-  opacity:0,
-  scale:.95,
-  ease:"back.inOut"
+  opacity: 0,
+  scale: .95,
+  ease: "back.inOut"
 })
-.from([".productive-heading",".productive-subtext",".productive-cta"],{
-  y:80,
-  opacity:0,
-  stagger:.4
-})
-.to(".productive-cta",{
-  y:0,
-  opacity:1
-})
-.to(['.productive-img',".productive-heading",".productive-subtext",".productive-cta"],{
-  opacity:.7,
-})
+  .from([".productive-heading", ".productive-subtext", ".productive-cta"], {
+    y: 80,
+    opacity: 0,
+    stagger: .4
+  })
+  .to(".productive-cta", {
+    y: 0,
+    opacity: 1
+  })
+  .to(['.productive-img', ".productive-heading", ".productive-subtext", ".productive-cta"], {
+    opacity: .7,
+  })
 
-gsap.from('.testimonial',{
-  y:50,
+gsap.from('.testimonial', {
+  y: 50,
   opacity: 0,
   stagger: .3,
   scrollTrigger: {
@@ -103,9 +103,9 @@ gsap.from('.testimonial',{
     scrub: true,
     markers: true
   },
-  onComplete : () => {
-    gsap.to('.testimonial',{
-      y:20,
+  onComplete: () => {
+    gsap.to('.testimonial', {
+      y: 20,
       duration: 2,
       yoyo: true,
       repeat: -1,
@@ -126,14 +126,27 @@ const notifyTl = gsap.timeline({
 })
 
 
-notifyTl.from('.notify',{
+notifyTl.from('.notify', {
   width: 0,
-  height:0,
-  padding:0,
+  height: 0,
+  padding: 0,
   duration: 1
 })
-.from([".notify-heading",".notify-subtext", ".notify-form"],{
-  y:30,
+  .from([".notify-heading", ".notify-subtext", ".notify-form"], {
+    y: 30,
+    opacity: 0,
+    stagger: .3
+  })
+
+
+  
+gsap.from(".footer-item", {
   opacity: 0,
-  stagger: .3
-})
+  y: 20,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "footer",
+    start: "top 90%",
+    marker: true,
+  }
+});
