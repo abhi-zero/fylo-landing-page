a2eda2d68898291634b1928dae49c7b474724eb9
abhi-zero <abhi.0zero@gmail.com>
2025-11-08 16:06:11 +0530

    feat: add scrolltrigger and animation for sections

--- src/anime.js
+++ src/anime.js
@@ -1,4 +1,7 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const navIntroTl = gsap.timeline({
   defaults: {
@@ -40,7 +43,97 @@ navIntroTl
     opacity:0
   },"-=.2")
   .fromTo(".cta",
-  { y: 30, opacity: 0 },
-  { y: 0, opacity: 1, duration: .5 },
-  "-=.2"
+  { y: 60, opacity: 0 },
+  { y: 0, opacity: 1, duration: 1 }
 );
+
+
+
+gsap.from(".feature", {
+  y:50,
+  opacity: 0,
+  stagger: .3,
+  scrollTrigger: {
+    trigger: ".features",
+    start: "top 80%",
+    end: "bottom 70%",
+    scrub: .6,
+    markers: false
+  }
+})
+
+const productivityTL = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".productivity",
+    start: "top 15%",
+    end: "+=150%",
+    scrub: .7,
+    pin: true,
+    markers: true,
+  }
+})
+
+productivityTL.from('.productive-img',{
+  x: -60,
+  opacity:0,
+  scale:.95,
+  ease:"back.inOut"
+})
+.from([".productive-heading",".productive-subtext",".productive-cta"],{
+  y:80,
+  opacity:0,
+  stagger:.4
+})
+.to(".productive-cta",{
+  y:0,
+  opacity:1
+})
+.to(['.productive-img',".productive-heading",".productive-subtext",".productive-cta"],{
+  opacity:.7,
+})
+
+gsap.from('.testimonial',{
+  y:50,
+  opacity: 0,
+  stagger: .3,
+  scrollTrigger: {
+    trigger: ".testimonials",
+    start: "top 80%",
+    end: "bottom 60%",
+    scrub: true,
+    markers: true
+  },
+  onComplete : () => {
+    gsap.to('.testimonial',{
+      y:20,
+      duration: 2,
+      yoyo: true,
+      repeat: -1,
+      ease: "sine.inOut",
+      stagger: .5
+    })
+  }
+})
+
+
+const notifyTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".notify-section",
+    start: "top 80%",
+    end: "bottom 40%",
+    scrub: .4,
+  }
+})
+
+
+notifyTl.from('.notify',{
+  width: 0,
+  height:0,
+  padding:0,
+  duration: 1
+})
+.from([".notify-heading",".notify-subtext", ".notify-form"],{
+  y:30,
+  opacity: 0,
+  stagger: .3
+})
